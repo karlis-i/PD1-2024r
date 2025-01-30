6cc64a0ad6aa161aebaf62851ee8e6f60d634b73
kovaadri <s23kovaadri@venta.lv>
2025-01-30 14:02:05 +0200

    Fixed W3C, added comments/sources

--- s23kovaadri/script.js
+++ s23kovaadri/script.js
@@ -1,19 +1,24 @@
 function scrollGallery(target) {
     let i = 0;
 
-    // set initial image + text
+    // Set initial image + text.
     target.style.backgroundImage = `url('images/${images[i]}')`;
     let text_target = document.getElementById(images[i].substring(0, images[i].length - 4));
+
     text_target.classList.add("visible");
     target.classList.add("visible");
     text_target.style.display = "block";
     
-    // loop through rest
+    // Loop through rest.
     setInterval(() => {
+        // Plays fade out animation.
         text_target.classList.remove("visible");
         target.classList.remove("visible");
 
+        // Hide previously active text after animation finishes.
         setTimeout(() => {
+            // Changing opacity instead of 'display' makes the text stack, making a massive text container.
+            // As changing opacity doesn't fully hide the text, we make use of 'display' to remove it.
             text_target.style.display = "none";
         }, 1000);
 
@@ -22,16 +27,21 @@ function scrollGallery(target) {
             i = 0;
         }
 
+        // Set next image + text entry
         setTimeout(() => {
             target.style.backgroundImage = `url('images/${images[i]}')`;
             text_target = document.getElementById(images[i].substring(0, images[i].length - 4));
             text_target.style.display = "block";
+            
+            // Plays fade in animation
             setTimeout(() => {
+                // Source: ChatGPT. If element's 'display' property is set to 'block' as the opacity changes then animation doesn't work.
+                // To circumvent this we append the class responsible for the animation a non-specific time after setting 'display'. 
                 text_target.classList.add("visible");
                 target.classList.add("visible");
-            }, 10); // add a delay so the fade animation doesn't play when the object doesn't exist
-        }, 1000); // length of anim
-    }, 16000); // interval between gallery entries
+            }, 10);
+        }, 1000); // Length of animation.
+    }, 16000); // Interval between gallery entries.
 }
 
 images = ['office.png', 'ups.jpg', 'athlete.png', 'reporter.png', 'security.jpg'];
